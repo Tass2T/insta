b0b6350b9ceba61b8dc376e08d680dffa87cd12a
Tass2T <griveau.bruno@gmail.com>
2024-04-27 00:55:39 +0200

    added tailwinds

---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="h-full w-52 border-r-[1px] border-neutral-800 px-3 pt-2 pb-5 flex flex-col">
+    <div class="h-12 mb-5 flex items-center text-white pl-3">Instagram</div>
+    <div class="h-full">
+      <div></div>
+      <div></div>
+    </div>
+  </main>
+</template>
